6a8372ec193832fcdaa54d03378901e91882c465
DenisKorolev <valveufa@gmail.com>
2018-04-07 00:03:28 +0500

    Wrappers were fixed

--- src/main/java/ru/bellintegrator/practice/common/advice/ResponseControllerAdvice.java
+++ src/main/java/ru/bellintegrator/practice/common/advice/ResponseControllerAdvice.java
@@ -4,22 +4,22 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import ru.bellintegrator.practice.common.view.DataView;
 import ru.bellintegrator.practice.common.view.ErrorView;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "ru.bellintegrator.practice")
 public class ResponseControllerAdvice implements ResponseBodyAdvice {
     @Override
     public boolean supports(MethodParameter methodParameter, Class aClass) {
-        
-        return false;
+
+        return true;
     }
 
     @Override
     public Object beforeBodyWrite(Object o, MethodParameter methodParameter, MediaType mediaType, Class aClass, ServerHttpRequest serverHttpRequest, ServerHttpResponse serverHttpResponse) {
+
         if (o instanceof ErrorView)
             return o;
         else
